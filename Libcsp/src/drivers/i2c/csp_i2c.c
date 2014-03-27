c74e65c32346680b7a55d3c78e9ec520db0b3987
carlgonz <contacto.carlosgonzalez@gmail.com>
2014-03-27 12:09:19 -0300

    [i2c][trx][tested] csp_i2c_init do not open i2c_port. Done in pic_pc104_config.

--- Libcsp/src/drivers/i2c/csp_i2c.c
+++ Libcsp/src/drivers/i2c/csp_i2c.c
@@ -23,10 +23,10 @@ int i2c_init(int handle, int mode, uint8_t addr, uint16_t speed, int queue_len_t
     switch(handle)
     {
         case 0:
-            i2c1_open(BRG, (char)addr);
+//            i2c1_open(BRG, (char)addr);
             break;
         case 1:
-            i2c2_open(BRG, (char)addr);
+//            i2c2_open(BRG, (char)addr);
             break;
         default:
             return CSP_ERR_DRIVER;
@@ -41,32 +41,27 @@ int i2c_send(int handle, i2c_frame_t * frame, uint16_t timeout)
     if(handle != 0)
         return CSP_ERR_DRIVER;
     
-#ifdef SCH_GRL_VERBOSE
+#if SCH_I2C_SUCHAI_VERBOSE
     printf("To: %d. Size: %d. Data: ", frame->dest, frame->len);
     int i; for(i=0; i<frame->len; i++) printf("0x%X,", frame->data[i]); printf("\n");
 #endif
 
-    char d_address[] = {I2C_EEPROM_ID, 0};
-//    char d_address[] = {0xAB, 0};
-    int d_address_len = 2; //Don't use register address, only device address
+    char d_address[] = {0x05, 0};
+    int d_address_len = 1; //Don't use register address, only device address
 
     //Send frame via I2C1
     int total = i2c1_master_fputs((const char*)(frame->data), frame->len, d_address, d_address_len);
 
     if(total == frame->len)
     {
-#ifdef SCH_GRL_VERBOSE
+#if SCH_I2C_SUCHAI_VERBOSE
         printf("Success in I2C driver (%d of %d bytes transmited)\n", total, frame->len);
-//        char recv[frame->len];
-//        total = i2c1_master_fgets(recv, frame->len, d_address, 2);
-//        printf("Received: ");
-//        for(i=0; i<frame->len; i++) printf("%c,", recv[i]); printf("\n");
 #endif
         return E_NO_ERR;
     }
     else
     {
-#ifdef SCH_GRL_VERBOSE
+#if SCH_I2C_SUCHAI_VERBOSE
         printf("Error in I2C driver (%d of %d bytes transmited)\n", total, frame->len);
 #endif
         return CSP_ERR_DRIVER;

b3a24b18ee2a86b7a713a303012ae73ea896b228
Tomas Opazo <tomas.opazo.t@gmail.com>
2016-01-19 09:34:59 -0300

    Updating TRX flight settings

--- SUCHAI_config.h
+++ SUCHAI_config.h
@@ -120,7 +120,7 @@
 /* Various aspects */
 /******************************************************************************/
 /* Realtime config */
-#define SCH_THOUSEKEEPING_SILENT_REALTIME   (0)  ///< 1=Realtime 0=Debugtime
+#define SCH_THOUSEKEEPING_SILENT_REALTIME   (1)  ///< 1=Realtime 0=Debugtime
 #define SCH_THOUSEKEEPING_ANT_DEP_REALTIME  (1)  ///< 1=Realtime 0=Debugtime
 #define SCH_THOUSEKEEPING_REALTIME          (1)  ///< 1=Realtime 0=Debugtime
 #define SCH_TFLIGHTPLAN_REALTIME            (1)  ///< 1=Realtime 0=Debugtime
@@ -134,12 +134,12 @@
 #define SCH_I2C3_ADDR           (0xF4) ///< I2C 3 Slave address
 
 /* TRX and COMM configs */
-#define SCH_TRX_BEACON_PERIOD              (30)   ///< [s] Periodo del beacon en segundos (UINT16)
+#define SCH_TRX_BEACON_PERIOD              (2*60)   ///< [s] Periodo del beacon en segundos (UINT16)
 #define SCH_TRX_BEACON_WPM                 (20)     ///< Velocidad del beacon en palabras por minuto 1-255
 #define SCH_TRX_BEACON_BAT_LVL             (500)    ///< Nivel de bateria minimo para transmitir beacon
 #define SCH_TRX_BEACON_MODE                (0)      ///< Tipo de beacon 0=CW, 1=Cw+FM, 2=FM
-#define SCH_TRX_BEACON_POSPONE_PRE         (1*60)   ///< def:30*60 [s] Periodo de inactividad del beacon luego del 1er encendido (silencio radial) (UINT16)
-#define SCH_TRX_BEACON_POSPONE_NOM         (1*60)   ///< [s] Periodo de inactividad del beacon luego de cada reinicio (excepto el 1ero) (UINT16)
+#define SCH_TRX_BEACON_POSPONE_PRE         (5*60)   ///< def:30*60 [s] Periodo de inactividad del beacon luego del 1er encendido (silencio radial) (UINT16)
+#define SCH_TRX_BEACON_POSPONE_NOM         (5*60)   ///< [s] Periodo de inactividad del beacon luego de cada reinicio (excepto el 1ero) (UINT16)
 #define SCH_TRX_RX_BAUD                    (12)     /// RX Baurade 12=1200bps, 24=2400bps, 48=4800bps [48 default]
 #define SCH_TRX_TX_BAUD                    (12)     /// TX Baurade 12=1200bps, 24=2400bps, 48=4800bps [48 default]
 #define SCH_TRX_NODE_GND                   (10)     /// Nodo de la estacion terrena
